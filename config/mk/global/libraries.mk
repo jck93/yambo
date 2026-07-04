2baea98ed775e5855c389e6a79b6916ca375d702
Andrea Ferretti <andrea.ferretti@nano.cnr.it>
2026-07-04 23:27:35 +0000

    p2y compilation fixed

--- config/mk/global/libraries.mk
+++ config/mk/global/libraries.mk
@@ -56,9 +56,9 @@ PJ_NLLIBS_LD = $(BASIC_LIBS_LD) interpolate real_time_control qp_control setup \
 #=====================================
 #
 2Y_LIBS       = driver tools modules memory allocations matrices linear_algebra parallel parser communicate output common timing Yio io $(IO_MODE) \
-                setup interface stop_and_restart bz_ops 
+                setup interface stop_and_restart wf_and_fft bz_ops
 2Y_LIBS_LD    = driver tools memory allocations communicate modules matrices linear_algebra parallel parser output common timing Yio io $(IO_MODE) \
-                setup interface stop_and_restart bz_ops 
+                setup interface stop_and_restart wf_and_fft bz_ops
 #
 # YPP folders 
 #===============
